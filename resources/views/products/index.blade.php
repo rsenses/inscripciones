093f4eb79d36480ba14ffbcf8cf29bc7e79f0714
José Yuste <jose.yuste@metech.es>
2021-03-10 12:45:17 +0100

    Lista de productos

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h2>Productos <small><a href="{{ route('products.create') }}">Nuevo producto</a></small></h2>
-        <x-products.table :products="$products"/>
+<div class="container">
+    <div class="mb-3">
+    <a class="btn btn-primary text-white pull-right d-block" data-toggle="tooltip" data-placement="bottom" title="Crear nuevo" href="{{ route('products.create') }}">
+            <i class="ion ion-plus"></i>
+        </a>
+        <div class="clearfix"></div>
     </div>
+
+
+    <div class="card bg-light">
+        <div class="card-body">
+        <h2 class="card-title text-primary">Productos</h2>
+            <x-products.table :products="$products" />
+        </div>
+    </div>
+
+</div>
 @endsection

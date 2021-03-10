6ced95efba942e4f9ceae945e6a298ca1e2d2c2d
José Yuste <jose.yuste@metech.es>
2021-03-10 12:21:47 +0100

    dashboard en card

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,10 +2,26 @@
 
 @section('content')
     <div class="container">
-        <h2>Productos activos</h2>
-        <x-products.table :products="$products"/>
 
-        <h2 class="mt-5">Últimas inscripciones</h2>
+    <div class="card text-white bg-light mb-5">
+      <div class="card-body">
+        <h2 class="card-title text-primary">Productos activos</h2>
+        <div class="table-responsive">
+            <x-products.table :products="$products"/>
+        </div>
+        </p>
+      </div>
+    </div>
+
+
+<div class="card text-white bg-light">
+  <div class="card-body">
+    <h2 class="card-title text-primary">Últimas inscripciones</h2>
+    <div class="table-responsive">
         <x-registrations.table :registrations="$registrations"/>
+    </div>
+  </div>
+</div>
+       
     </div>
 @endsection

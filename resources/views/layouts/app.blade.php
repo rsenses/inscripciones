733a7f2fa31f432747a8e85f29908316a4c0692d
José Yuste <jose.yuste@metech.es>
2021-03-10 12:25:20 +0100

    Menú cambiado y tooltips

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,106 +1,135 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        <div id="app">
-            <div class="d-flex" id="wrapper">
-                @auth
-                    <!-- Sidebar -->
-                    <div class="bg-light border-right" id="sidebar-wrapper">
-                        <div class="sidebar-heading">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <div class="d-flex" id="wrapper">
+            @auth
+                <!-- Sidebar -->
+                <div class="bg-dark border-right" id="sidebar-wrapper">
+                    <div class="sidebar-heading">
                         {{ config('app.name', 'Laravel') }}
-                        </div>
-                        <div class="list-group list-group-flush">
-                            <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-                            <a href="{{ route('products.index') }}" class="list-group-item list-group-item-action bg-light">Productos</a>
-                            <a href="{{ route('registrations.index') }}" class="list-group-item list-group-item-action bg-light">Inscripciones</a>
-                            <a href="{{ route('users.index', ['role' => 'customer']) }}" class="list-group-item list-group-item-action bg-light">Usuarios</a>
-                            <a href="{{ route('partners.index') }}" class="list-group-item list-group-item-action bg-light">Cabeceras</a>
-                            <a href="{{ route('invoices.index') }}" class="list-group-item list-group-item-action bg-light">Financiero</a>
-                        </div>
                     </div>
-                    <!-- /#sidebar-wrapper -->
-                @endauth
+                    <div class="list-group list-group-flush">
+                        <a href="{{ route('dashboard') }}"
+                            class="list-group-item list-group-item-action bg-light"><i class="ion ion-speedometer"
+                                aria-hidden="true"></i>&emsp;Dashboard</a>
+                        <a href="{{ route('products.index') }}"
+                            class="list-group-item list-group-item-action bg-light"><i class="ion ion-briefcase"
+                                aria-hidden="true"></i>&emsp;Productos</a>
+                        <a href="{{ route('registrations.index') }}"
+                            class="list-group-item list-group-item-action bg-light"><i class="ion ion-pricetags"
+                                aria-hidden="true"></i>&emsp;Inscripciones</a>
+                        <a href="{{ route('users.index', ['role' => 'customer']) }}"
+                            class="list-group-item list-group-item-action bg-light"><i class="ion ion-person-stalker"
+                                aria-hidden="true"></i>&emsp;Usuarios</a>
+                        <a href="{{ route('partners.index') }}"
+                            class="list-group-item list-group-item-action bg-light"><i class="ion ion-filing"
+                                aria-hidden="true"></i>&emsp;Cabeceras</a>
+                        <a href="{{ route('invoices.index') }}"
+                            class="list-group-item list-group-item-action bg-light"><i class="ion ion-cash"
+                                aria-hidden="true"></i>&emsp;Financiero</a>
+                    </div>
+                </div>
+                <!-- /#sidebar-wrapper -->
+            @endauth
 
-                <!-- Page Content -->
-                <div id="page-content-wrapper">
-                    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                        <div class="container-fluid">
-                            @auth
-                                <button class="btn btn-primary" id="menu-toggle">
-                                    <i class="fa fa-bars"></i>
-                                </button>
-                            @endauth
-
-                            <a class="navbar-brand" href="{{ url('/') }}">
-                            </a>
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                                <span class="navbar-toggler-icon"></span>
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+                <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                    <div class="container-fluid">
+                        @auth
+                            <button class="btn btn-link" id="menu-toggle">
+                                <i class="ion ion-android-more-vertical"></i>
                             </button>
+                        @endauth
 
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <!-- Left Side Of Navbar -->
-                                <ul class="navbar-nav mr-auto">
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-                                </ul>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <!-- Left Side Of Navbar -->
+                            <ul class="navbar-nav mr-auto">
 
-                                <!-- Right Side Of Navbar -->
-                                <ul class="navbar-nav ml-auto">
-                                    <!-- Authentication Links -->
-                                    @auth
-                                        <li class="nav-item dropdown">
-                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                                {{ Auth::user()->name }}
+                            </ul>
+
+                            <!-- Right Side Of Navbar -->
+                            <ul class="navbar-nav ml-auto">
+                                <!-- Authentication Links -->
+                                @auth
+                                    <li class="nav-item dropdown">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }}
+                                        </a>
+
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                                onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
                                             </a>
 
-                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                                                         onclick="event.preventDefault();
-                                                                                  document.getElementById('logout-form').submit();">
-                                                    {{ __('Logout') }}
-                                                </a>
-
-                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                                    @csrf
-                                                </form>
-                                            </div>
-                                        </li>
-                                    @endauth
-                                </ul>
-                            </div>
+                                            <form id="logout-form" action="{{ route('logout') }}"
+                                                method="POST" class="d-none">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
+                                @endauth
+                            </ul>
                         </div>
-                    </nav>
+                    </div>
+                </nav>
 
-                    <main class="py-4">
-                        @yield('content')
-                    </main>
-                </div>
+                <main class="py-4">
+                    @yield('content')
+                </main>
             </div>
         </div>
-        <script>
-            document.getElementById('menu-toggle').onclick = function() {
-                var element = document.getElementById('wrapper');
-                element.classList.toggle('toggled');
+    </div>
+    <script>
+        document.getElementById('menu-toggle').onclick = function () {
+            var element = document.getElementById('wrapper');
+            element.classList.toggle('toggled');
+            var menuIcon = $(this).children('i');
+            if (menuIcon.hasClass('ion-android-more-vertical') && !$('wrapper').hasClass('toggle')) {
+                menuIcon.removeClass('ion-android-more-vertical').addClass('ion-android-more-horizontal')
+            } else {
+                menuIcon.removeClass('ion-android-more-horizontal').addClass('ion-android-more-vertical')
             }
-        </script>
-        @yield('scripts')
-    </body>
+        }
+        window.onload =
+            function () {
+                $('[data-toggle="tooltip"]').tooltip();
+            }
+
+    </script>
+    @yield('scripts')
+</body>
+
 </html>

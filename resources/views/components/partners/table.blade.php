a999728d8c7f6ab639c5b974e5978e9f51280f3f
José Yuste <jose.yuste@metech.es>
2021-03-15 12:16:57 +0100

    alinear rows verticalmente en partners

--- resources/views/components/partners/table.blade.php
+++ resources/views/components/partners/table.blade.php
@@ -10,11 +10,11 @@
         <tbody>
             @foreach($partners as $partner)
                 <tr>
-                    <td>{{ $partner->name }}</td>
+                    <td style="vertical-align: middle;"> {{ $partner->name }}</td>
                     <td>
                         <img src="{{ asset('storage/' . $partner->image) }}" alt="{{ $partner->name }}" class="img-fuid" style="max-width: 200px;">
                     </td>
-                    <td  class="bg-primary text-center">
+                    <td  class="bg-primary text-center" style="vertical-align: middle;">
                         <a class="text-light " href="{{ route('partners.show', ['partner' => $partner]) }}">
                         <i class="ion ion-ios-eye" aria-hidden="true"></i> Ver
                         </a>

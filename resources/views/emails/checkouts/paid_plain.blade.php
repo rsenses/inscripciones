d7e2f2a5169f9f9e278bd93a6a707455a8437f16
Rubén Silva <ruben.silva@expomark.es>
2021-04-22 12:46:47 +0200

    Modifica email de bienvenida

--- resources/views/emails/checkouts/paid_plain.blade.php
+++ resources/views/emails/checkouts/paid_plain.blade.php
@@ -1,10 +1,10 @@
 Estimado D./ Dña. {{ $checkout->user->full_name }}
 
-Bienvenido al {{ $checkout->product->name }}.
+Le damos la bienvenida al {{ $checkout->product->name }}.
 
 Su inscripción ha sido confirmada y ya puede acceder a la zona privada del site del Foro.
 
-A través del siguiente enlace podrá acceder a toda la información relativa al encuentro:
+A través del siguiente enlace podrá acceder a toda la información relativa al evento:
 https://foro.expansion.com
 
 • Cómo llegar.
@@ -14,7 +14,7 @@ https://foro.expansion.com
 • Zona de preguntas en directo.
 • Actualizaciones de última hora.
 
-Incluir evento en mi calendario: {{ route('calendar.show', ['product' => $checkout->product]) }}
+Incluir en mi calendario: {{ route('calendar.show', ['product' => $checkout->product]) }}
 
 Esperamos que disfrute del evento,
 Relaciones Institucionales

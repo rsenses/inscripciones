799b631fa4274199b7d98eeb2783b6f6c978190b
Rubén Silva <ruben.silva@expomark.es>
2021-04-26 15:16:40 +0200

    Mas cambios en texto

--- resources/views/emails/checkouts/cancelled_plain.blade.php
+++ resources/views/emails/checkouts/cancelled_plain.blade.php
@@ -2,7 +2,7 @@ Estimad@ {{ $checkout->user->full_name }}
 
 Hemos iniciado el proceso de cancelación de asistencia al {{ $checkout->product->name }}. En unos días recibirá la factura de abono.
 
-Una vez recibida dicha factura procederemos a realizar la devolución del importe por el mismo método de pago con el que se realizó la inscripción, en el plazo máximo de 15 días.
+Una vez recibida dicha factura, procederemos a realizar la devolución del importe por el mismo método de pago con el que se realizó la inscripción en el plazo máximo de 15 días.
 
 Atentamente,
 
--- resources/views/emails/registrations/denied_plain.blade.php
+++ resources/views/emails/registrations/denied_plain.blade.php
@@ -2,7 +2,7 @@ Estimad@ {{ $registration->user->full_name }}
 
 Sentimos comunicarle que el aforo está completo y no es posible confirmar su inscripción.  
 
-Si finalmente quedara alguna plaza libre nos pondremos en contacto con usted. 
+Si finalmente quedara alguna plaza libre, nos pondremos en contacto con usted. 
 
 Muchas gracias por su interés. 
 

69e95dd7069dd6696df58d87788991bf59eca516
Rubén Silva <ruben.silva@expomark.es>
2021-04-26 11:10:16 +0200

    Corrige error de emails

--- resources/views/emails/registrations/created_plain.blade.php
+++ resources/views/emails/registrations/created_plain.blade.php
@@ -2,7 +2,7 @@ Estimad@ {{ $registration->user->full_name }}
 
 Su solicitud para asistir al {{ $registration->product->name }} ha sido registrada correctamente.
 
-En breve nos pondremos en contacto con usted para confirmar si su solicitud {{ $checkout->product->mode === 'online' ? 'ON LINE' : 'PRESENCIAL' }} ha sido aceptada.
+En breve nos pondremos en contacto con usted para confirmar si su solicitud {{ $registration->product->mode === 'online' ? 'ON LINE' : 'PRESENCIAL' }} ha sido aceptada.
 
 Le recordamos que el aforo es limitado y por riguroso orden de inscripción.
 

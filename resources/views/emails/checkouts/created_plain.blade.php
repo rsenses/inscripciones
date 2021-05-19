6c1eda2633a33fe34a4a0c4e67d7c854bdafb458
Rubén Silva <ruben.silva@expomark.es>
2021-05-19 15:41:56 +0200

    Añade codigo premium

--- resources/views/emails/checkouts/created_plain.blade.php
+++ resources/views/emails/checkouts/created_plain.blade.php
@@ -2,7 +2,7 @@ Estimad@ {{ $checkout->user->full_name }}
 
 Su solicitud para asistir al {{ $checkout->product->name }} ha sido aceptada.
 
-@if($promo === 'SUSCRIPTOR')
+@if($promo === 'SUSCRIPTOR' || $promo === 'PREMIUM')
 Para beneficiarse por ser suscriptor, incorpore el siguiente código de descuento durante el proceso de pago: <b>{{ $discount->code }}</b> y se le aplicará un descuento del {{ $discount->quantity }}%.
 @endif
 

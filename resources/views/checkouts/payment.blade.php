e14473200beee45b317178f03270a6bede1f273e
Rubén Silva <ruben.silva@expomark.es>
2021-07-21 08:56:26 +0200

    Transferencia solo si el pago es mas de 100

--- resources/views/checkouts/payment.blade.php
+++ resources/views/checkouts/payment.blade.php
@@ -80,24 +80,26 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6">
-                <div class="card bg-light">
-                    <div class="card-body">
-                        <p class="text-center"><strong>CON TRANSFERENCIA desde tu banco:</strong></p>
-                        <p>
-                        <strong>Beneficiario</strong>: U.E.INFOR. ECONÓM. S.L.U<br>
-                        <strong>Nombre del Banco</strong>: Bankinter<br>
-                        <strong>Cuenta</strong>: 42 0128 6035 77 0100000587<br>
-                        <strong>IBAN</strong>: ES 42 0128 6035 77 0100000587<br>
-                        <strong>BIC</strong>: SWIFT BKBKESMMXXX<br>
-                        <strong>Concepto de transferencia</strong>: Asistencia {{ $checkout->product->mode === 'online' ? 'Online' : 'Presencial' }} {{ $checkout->user->full_name }} {{ $checkout->invoice->number }}<br>
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="{{ route('tpv.success', ['checkout' => $checkout, 'method' => 'transfer']) }}" class="btn btn-success btn-block btn-lg"> Pagar por transferencia</a>
+            @if($checkout->amount > 100)
+                <div class="col-lg-6">
+                    <div class="card bg-light">
+                        <div class="card-body">
+                            <p class="text-center"><strong>CON TRANSFERENCIA desde tu banco:</strong></p>
+                            <p>
+                            <strong>Beneficiario</strong>: U.E.INFOR. ECONÓM. S.L.U<br>
+                            <strong>Nombre del Banco</strong>: Bankinter<br>
+                            <strong>Cuenta</strong>: 42 0128 6035 77 0100000587<br>
+                            <strong>IBAN</strong>: ES 42 0128 6035 77 0100000587<br>
+                            <strong>BIC</strong>: SWIFT BKBKESMMXXX<br>
+                            <strong>Concepto de transferencia</strong>: Asistencia {{ $checkout->product->mode === 'online' ? 'Online' : 'Presencial' }} {{ $checkout->user->full_name }} {{ $checkout->invoice->number }}<br>
+                            </p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="{{ route('tpv.success', ['checkout' => $checkout, 'method' => 'transfer']) }}" class="btn btn-success btn-block btn-lg"> Pagar por transferencia</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 @endsection

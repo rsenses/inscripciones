5e917202024d52efa223852c6103ef440a685b7e
Rubén Silva <ruben.silva@expomark.es>
2021-07-29 13:11:04 +0200

    Cambia texto aceptación

--- resources/views/emails/telva/checkouts/created_plain.blade.php
+++ resources/views/emails/telva/checkouts/created_plain.blade.php
@@ -2,7 +2,7 @@ Estimad@ {{ $checkout->user->full_name }}
 
 Tu solicitud para asistir al {{ $checkout->product->name }} ha sido completada correctamente.
 
-Para proceder al pago y confirmar tu asistencia {{ $checkout->product->mode === 'online' ? 'ON LINE' : 'PRESENCIAL' }} regístrese en el siguiente enlace y complete tu inscripción:
+Para proceder al pago y confirmar tu asistencia {{ $checkout->product->mode === 'online' ? 'ON LINE' : 'PRESENCIAL' }} regístrate en el siguiente enlace y completa tu inscripción:
           
 https://inscripciones.telva.com/checkouts/{{ $checkout->id }}?t={{ $checkout->token }}
 

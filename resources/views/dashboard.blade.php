1275b7ca57c20149ede78884f17b5fe4f55e0c89
Rubén Silva <ruben.silva@rsenses.com>
2022-02-10 17:18:40 +0100

    Gilipoerror

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,27 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-
+<div class="container-fluid">
     <div class="card bg-white mb-5">
-      <div class="card-body">
-        <h4 class="card-title">Productos activos</h2>
-        <div class="table-responsive">
-            <x-products.table :products="$products"/>
+        <div class="card-body">
+            <h4 class="card-title">Productos activos</h4>
+            <div class="table-responsive">
+                <x-products.table :products="$products" />
+            </div>
         </div>
-        </p>
-      </div>
     </div>
 
-
-<div class="card bg-white">
-  <div class="card-body">
-    <h4 class="card-title">Inscripciones pendientes de valorar</h2>
-    <div class="table-responsive">
-        <x-registrations.table :registrations="$registrations"/>
+    <div class="card bg-white">
+        <div class="card-body">
+            <h4 class="card-title">Inscripciones pendientes de valorar</h4>
+            <div class="table-responsive">
+                <x-registrations.table :registrations="$registrations" />
+            </div>
+        </div>
     </div>
-  </div>
 </div>
-       
-    </div>
 @endsection

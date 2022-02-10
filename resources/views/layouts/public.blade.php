c378b87b2068fd94cbc7651b8cc47125abc6c9a2
Rubén Silva <ruben.silva@rsenses.com>
2022-02-10 17:17:16 +0100

    Añade layout public

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -88,111 +88,9 @@
          </script>
     @endif
     <div id="app">
-            <div class="page-loader">
-                <div class="loader text-primary">Cargando...</div>
-            </div>
         <div class="d-flex" id="wrapper">
-            @auth
-                <!-- Sidebar -->
-                <div class="bg-primary border-right" id="sidebar-wrapper">
-                    <div class="sidebar-heading">
-                        {{ config('app.name', 'Laravel') }}
-                    </div>
-                    <div class="list-group list-group-flush">
-                        <a href="{{ route('dashboard') }}"
-                            class="list-group-item list-group-item-action bg-white"><i
-                               class="lni lni-dashboard"></i>&emsp;Dashboard</a>
-                        <a href="{{ route('products.index') }}"
-                            class="list-group-item list-group-item-action bg-white"><i
-                               class="lni lni-briefcase"></i>&emsp;Productos</a>
-                        @if(Auth::user()->role === 'superadmin')
-                            <a href="{{ route('campaigns.index') }}" class="list-group-item list-group-item-action bg-white"><i class="ion ion-ios-analytics" aria-hidden="true"></i>&emsp;Campañas</a>
-                            <a href="{{ route('registrations.index') }}"
-                               class="list-group-item list-group-item-action bg-white"><i
-                                   class="lni lni-pencil-alt"></i>&emsp;Inscripciones</a>
-                            <a href="{{ route('users.index', ['role' => 'customer']) }}"
-                               class="list-group-item list-group-item-action bg-white"><i
-                                   class="lni lni-users"></i>&emsp;Usuarios</a>
-                            <a href="{{ route('partners.index') }}"
-                               class="list-group-item list-group-item-action bg-white"><i
-                                  class="lni lni-bookmark-alt"></i>&emsp;Cabeceras</a>
-                        @endif
-                        <a href="{{ route('invoices.index') }}"
-                            class="list-group-item list-group-item-action bg-white"><i
-                               class="lni lni-revenue"></i>&emsp;Financiero</a>
-                    </div>
-                </div>
-                <!-- /#sidebar-wrapper -->
-            @endauth
-
             <!-- Page Content -->
             <div id="page-content-wrapper">
-                <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                    <div class="container-fluid">
-                        @auth
-                            <button class="btn btn-link" id="menu-toggle">
-                                <i class="ion ion-android-more-vertical"></i>
-                            </button>
-                        @endauth
-
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                        </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <!-- Left Side Of Navbar -->
-
-
-                            <!-- Right Side Of Navbar -->
-                            <ul class="navbar-nav ml-auto">
-                                <li class="nav-item">
-                                    <div class="d-flex mx-4 align-content-center">
-                                        <i class="lni lni-sun mx-1 mt-3"></i>
-                                        <label class="switch">
-                                            <input type="checkbox" id="themeSelector" name="themeSelector">
-                                            <span class="slider round"></span>
-                                        </label>
-                                        <i class="lni lni-night mx-1 mt-3"></i>
-                                    </div>
-                                </li>
-                                <!-- Authentication Links -->
-                                @auth
-                                 <li class="nav-item">
-                                     <a class="nav-link"
-                                        href="#"
-                                        role="button"
-                                        v-pre>
-                                         {{ Auth::user()->name }} {{ Auth::user()->last_name }}
-                                     </a>
-                                 </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();" 
-                                         data-toggle="tooltip"
-                                        data-placement="bottom"
-                                        title=" {{ __('Logout') }}">
-                                            <i class="lni lni-exit"></i>
-                                        </a>
-                                        <form id="logout-form"
-                                            action="{{ route('logout') }}"
-                                            method="POST"
-                                            class="d-none">
-                                            @csrf
-                                        </form>
-                                </li>
-                                   
-  
-
-                                @endauth
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-
                 <main class="py-4">
                     @yield('content')
                 </main>
@@ -221,44 +119,19 @@
     </div>
 
     <script>
-        if (document.getElementById('menu-toggle')) {
-            document.getElementById('menu-toggle').onclick = function () {
-                var element = document.getElementById('wrapper');
-                element.classList.toggle('toggled');
-                var menuIcon = $(this).children('i');
-                if (menuIcon.hasClass('ion-android-more-vertical') && !$('wrapper').hasClass('toggle')) {
-                    menuIcon.removeClass('ion-android-more-vertical').addClass('ion-android-more-horizontal')
-                } else {
-                    menuIcon.removeClass('ion-android-more-horizontal').addClass('ion-android-more-vertical')
+        @if($domain === 'telva')
+            if (document.getElementById('redsys_form')) {
+                document.getElementById('redsys_form').onsubmit = function (e) {
+                    envioSC("telva energiayfelicidad | pagar con tarjeta");
                 }
             }
-        }
-        window.addEventListener('load',
-        function () {
-            $('[data-toggle="tooltip"]').tooltip();
-            $(".loader").fadeOut();
-            $(".page-loader").delay(150).fadeOut("fast");
-            if(!localStorage.getItem('theme')){
-                $('body').removeClass('dark');
-                $('#themeSelector').prop('checked', false)
-            }else{
-                $('body').addClass('dark')
-                $('#themeSelector').prop('checked', true)
+            if (document.getElementById('invoice-data')) {
+                document.getElementById('invoice-data').onsubmit = function (e) {
+                    envioSC("telva energiayfelicidad | proceder al pago");
+                }
             }
-
-            $('#themeSelector').change(function(e){
-                if( $('body').hasClass('dark')){
-                    $('body').removeClass('dark')
-                    localStorage.removeItem('theme')
-                 }else{
-                     $('body').addClass('dark')
-                     localStorage.setItem('theme', '{dark:true}')
-                 }
-            })
-        })
+        @endif
     </script>
     @yield('scripts')
-    
 </body>
-
 </html>

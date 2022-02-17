5add23c642edbd1f6cf10427db06c736b3c85635
Jose Yuste <jose.yuste@gmail.com>
2022-02-17 09:06:07 +0100

    arreglar entradas

--- resources/views/tickets/checkout.blade.php
+++ resources/views/tickets/checkout.blade.php
@@ -65,11 +65,11 @@
                 <h2>{{ $registration->user->full_name }}</h2>
                 <span>Nombre</span>
             </div>
+            <div class="place" style="float: none">
             @if($registration->product->mode === 'presencial')
             <h2>{{ $registration->product->place }}</h2>
             <span>Lugar</span>
             @else
-            <div class="place" style="float: none">
                 <h2> <a href="{{ $checkout->campaign->url }}" target="_blank">{{ $checkout->campaign->url }}</a></h2>
                 <span>URL</span>
                 @endif
@@ -144,7 +144,7 @@
                             </g>
                         </g>
                     </svg>
-                    {{ $registration->unique_id }}}</a>
+                    {{ $registration->unique_id }}</a>
                 @else
                 {{ SimpleSoftwareIO\QrCode\Facades\QrCode::color(...$brand['color'])->format('svg')->generate($registration->unique_id) }}
                 <p>{{ $registration->unique_id }}</p>
@@ -173,9 +173,10 @@
         @else
         <li>
             <a href="{{ route('tickets.show', [$registration, $registration->unique_id]) }}">
-                <svg fill="none" stroke="currentColor" style="height:30px;max-height:30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30px" height="30px">
-                    <path d="M 23 3 A 4 4 0 0 0 19 7 A 4 4 0 0 0 19.09375 7.8359375 L 10.011719 12.376953 A 4 4 0 0 0 7 11 A 4 4 0 0 0 3 15 A 4 4 0 0 0 7 19 A 4 4 0 0 0 10.013672 17.625 L 19.089844 22.164062 A 4 4 0 0 0 19 23 A 4 4 0 0 0 23 27 A 4 4 0 0 0 27 23 A 4 4 0 0 0 23 19 A 4 4 0 0 0 19.986328 20.375 L 10.910156 15.835938 A 4 4 0 0 0 11 15 A 4 4 0 0 0 10.90625 14.166016 L 19.988281 9.625 A 4 4 0 0 0 23 11 A 4 4 0 0 0 27 7 A 4 4 0 0 0 23 3 z" />
-                </svg></a>
+               <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+width="30" height="30" style="height:30px;max-height:30px"
+viewBox="0 0 30 30"
+fill="currentColor" stroke="none"><path d="M 23 3 A 4 4 0 0 0 19 7 A 4 4 0 0 0 19.09375 7.8359375 L 10.011719 12.376953 A 4 4 0 0 0 7 11 A 4 4 0 0 0 3 15 A 4 4 0 0 0 7 19 A 4 4 0 0 0 10.013672 17.625 L 19.089844 22.164062 A 4 4 0 0 0 19 23 A 4 4 0 0 0 23 27 A 4 4 0 0 0 27 23 A 4 4 0 0 0 23 19 A 4 4 0 0 0 19.986328 20.375 L 10.910156 15.835938 A 4 4 0 0 0 11 15 A 4 4 0 0 0 10.90625 14.166016 L 19.988281 9.625 A 4 4 0 0 0 23 11 A 4 4 0 0 0 27 7 A 4 4 0 0 0 23 3 z"></path></svg></a>
         </li>
         @endif
     </ul>

9d47d2779bc7170473613fe9fb0fdcf0d24d4d0f
Rubén Silva <ruben.silva@rsenses.com>
2022-02-21 17:03:57 +0100

    Añade evento de error de pago

--- resources/views/payments/error.blade.php
+++ resources/views/payments/error.blade.php
@@ -37,3 +37,13 @@
     </div>
 </div>
 @endsection
+
+@section('scripts')
+<script>
+    utag.link({
+        "event_category": "{{ transliterator_transliterate('Any-Latin; Latin-ASCII; Lower()', $checkout->campaign->name) }}",
+        "event_action": "{{ $checkout->campaign->short_name }}:error pago",
+        "event_label" : "{{ $checkout->campaign->short_name }}:{{ $checkout->tpv }}",
+    });
+</script>
+@endsection

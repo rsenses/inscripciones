aaf31cf0e8b062f68101cb679d573e932cd39514
Rubén Silva <ruben.silva@rsenses.com>
2022-02-23 08:45:06 +0100

    Modifica email de compra aceptada

--- resources/views/emails/energiayfelicidad/checkouts/accepted_plain.blade.php
+++ resources/views/emails/energiayfelicidad/checkouts/accepted_plain.blade.php
@@ -1,13 +1,14 @@
 Estimad@ {{ $checkout->user->full_name }}
 
-Solicitud completada correctamente. A continuación detallamos el resumen de la compra:
+Ya estás registrada en el {{ $checkout->campaign->name
+}}. A continuación detallamos el resumen de la compra:
 
 # Producto
 @foreach ($checkout->products->groupBy('id') as $product)
 {{ $product->count() }}x {{ $product[0]->name }} {{ $product[0]->mode }}
 @endforeach
 
-Para proceder al pago y confirmar tu asistencia completa tu inscripción en el siguiente enlace:
+Si no has completado tu compra, recuerda que puedes hacerlo en cualquier momento desde el siguiente enlace.:
 {{ route('checkouts', ['checkout' => $checkout, 't' => $checkout->token]) }}
 
 Atentamente,

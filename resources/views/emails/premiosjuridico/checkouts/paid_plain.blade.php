3f56e86642b5fce3315bd963fabbf01db063e7a7
Rubén Silva <ruben.silva@rsenses.com>
2022-03-28 14:44:51 +0200

    Añaded enlace a entradas en la compra

--- resources/views/emails/premiosjuridico/checkouts/paid_plain.blade.php
+++ resources/views/emails/premiosjuridico/checkouts/paid_plain.blade.php
@@ -2,6 +2,9 @@ Estimad@ {{ $checkout->user->full_name }}
 
 Le damos la bienvenida al {{ $checkout->campaign->name }}.
 
+Aquí tiene @if($checkout->quantity > 1)sus entradas @else su entrada @endif:
+{{ route('tickets.show.checkout', ['checkout' => $checkout, 'token' => $checkout->token]) }}
+
 A través del siguiente enlace podrá acceder a toda la información relativa al evento:
 https://premios.juridico.com
 

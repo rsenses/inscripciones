fa45da22a4ccdbf1a9ae68f02f85423c56a2041d
Rubén Silva <ruben.silva@rsenses.com>
2022-04-05 14:29:46 +0200

    Post checkoutCreated 10 registros mesa jurídico

---
+++ app/Listeners/CheckoutCreated.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\CheckoutCreated as CheckoutCreatedEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class CheckoutCreated implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  CheckoutCreatedEvent  $event
+     * @return void
+     */
+    public function handle(CheckoutCreatedEvent $event)
+    {
+        if ($event->checkout->products()->where('products.id', 16)->count()) {
+            $this->mesaJuridicoTenInscriptions($event->checkout);
+        }
+    }
+
+    private function mesaJuridicoTenInscriptions($checkout)
+    {
+        $registrations = $checkout->registrations;
+        $registration = $checkout->registrations[0];
+        $count = 10 - $registrations->count();
+
+        for ($i=1; $i <= $count; $i++) {
+            $newRegistration = $registration->replicate();
+            $newRegistration->save();
+        }
+    }
+}

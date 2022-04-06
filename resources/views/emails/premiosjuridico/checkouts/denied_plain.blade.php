625a61bba01bf0065a0846550122ab5cb79977c6
Rubén Silva <ruben.silva@rsenses.com>
2022-04-06 10:39:02 +0200

    Modifica emails ded juridico

--- resources/views/emails/premiosjuridico/checkouts/denied_plain.blade.php
+++ resources/views/emails/premiosjuridico/checkouts/denied_plain.blade.php
@@ -2,7 +2,7 @@ Estimad@ {{ $checkout->user->full_name }}
 
 Sentimos comunicarle que el aforo está completo y no es posible confirmar su inscripción.
 
-Si finalmente quedara alguna plaza libre, nos pondremos en contacto con usted.
+En este momento, le incluimos en lista de espera. Si finalmente quedara alguna plaza libre, nos pondremos en contacto con usted.
 
 Muchas gracias por su interés.
 
--- resources/views/emails/premiosjuridico/checkouts/paid_plain.blade.php
+++ resources/views/emails/premiosjuridico/checkouts/paid_plain.blade.php
@@ -8,12 +8,6 @@ Aquí tiene @if($checkout->quantity > 1)sus entradas @else su entrada @endif:
 A través del siguiente enlace podrá acceder a toda la información relativa al evento:
 https://premios.juridico.com
 
-• Cómo llegar.
-• Programa y ponentes.
-• Listado de asistentes.
-• Zona de preguntas en directo.
-• Actualizaciones de última hora.
-
 Incluir en mi calendario: {{ route('calendar.show', ['product' => $checkout->products[0]]) }}
 
 Atentamente,

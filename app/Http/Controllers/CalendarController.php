d2416e80392d560c5e155fc1ac7bf33f95dd9d93
Rubén Silva <ruben.silva@rsenses.com>
2022-04-08 10:26:41 +0200

    Añade mas datos al calendario

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -10,12 +10,16 @@ use Spatie\IcalendarGenerator\Components\Calendar;
 
 class CalendarController extends Controller
 {
-    public function show(Product $product) {
-        $calendar = Calendar::create($product->name)
-            ->event(Event::create($product->name)
+    public function show(Product $product)
+    {
+        $calendar = Calendar::create($product->campaign->name)
+            ->event(
+                Event::create($product->campaign->name)
+                ->description($product->description)
+                ->address($product->place)
                 ->startsAt($product->start_date)
                 ->endsAt($product->end_date)
-        );
+            );
 
         return response($calendar->get(), 200, [
            'Content-Type' => 'text/calendar',

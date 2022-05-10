3d1cbd2430f338133d9fe3404eefbdd137ae20bd
Rubén Silva <ruben.silva@rsenses.com>
2022-05-10 11:16:50 +0200

    Modifica emails de CF

--- resources/views/emails/jornadacf/checkouts/paid_plain.blade.php
+++ resources/views/emails/jornadacf/checkouts/paid_plain.blade.php
@@ -1,21 +1,16 @@
 Estimad@ {{ $checkout->user->full_name }}
 
-Te damos la bienvenida al {{ $checkout->campaign->name }}.
+Te damos la bienvenida a la {{ $checkout->campaign->name }}.
 
 @if(empty($checkout->mode()['online']))
 Aquí tienes tus entradas:
 {{ route('tickets.show.checkout', ['checkout' => $checkout, 'token' => $checkout->token]) }}
-@elseif(empty($checkout->mode()['presencial']))
-Podrás acceder al streaming mediante tu email y contraseña.
-@else
-Aquí tienes tus entradas:
-{{ route('tickets.show.checkout', ['checkout' => $checkout, 'token' => $checkout->token]) }}
 
-Podrás acceder al streaming mediante tu email y contraseña.
+Te esperamos el próximo 13 de junio a las 8.45 en el Salón Círculo Palace del Hotel Westin Palace de Madrid. Entrada por Plaza de Neptuno, 28014 Madrid.
 @endif
 
 A través del siguiente enlace podrás ver toda la información relativa al evento:
-https://energiayfelicidad.Correo Farmacéutico.com/
+https://jornadagestioncf.diariomedico.com/
 
 Incluir en mi calendario: {{ route('calendar.show', ['product' => $checkout->products[0]]) }}
 

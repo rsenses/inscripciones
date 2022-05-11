50532dfeb3f143840c7f488e47c44eb40d562c83
Rubén Silva <ruben.silva@rsenses.com>
2022-05-11 17:21:26 +0200

    Devuelve solo el id de youtube

--- app/Http/Controllers/Api/ImbfController.php
+++ app/Http/Controllers/Api/ImbfController.php
@@ -28,7 +28,7 @@ class ImbfController extends Controller
         }
 
         return [
-            'streaming' => '<iframe width="560" height="349" src="https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0&hd=1" frameborder="0" allowfullscreen ></iframe>'
+            'streaming' => 'dQw4w9WgXcQ'
         ];
     }
 }

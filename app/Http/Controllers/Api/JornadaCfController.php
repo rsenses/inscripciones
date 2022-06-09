73e2c95eee9a3ef268550ee14cac70290f9b5de3
Rubén Silva <ruben.silva@rsenses.com>
2022-06-09 12:14:19 +0200

    Añade streaming CF

--- app/Http/Controllers/Api/JornadaCfController.php
+++ app/Http/Controllers/Api/JornadaCfController.php
@@ -28,6 +28,8 @@ class JornadaCfController extends Controller
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
-        return response($user);
+        return response([
+            'streaming' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/AhxkIgbnkJ8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        ]);
     }
 }

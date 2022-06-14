103853d60d4c7edc8174b0f7d96df00be3adc503
Rubén Silva <ruben.silva@rsenses.com>
2022-06-14 14:12:28 +0200

    Añade validaciones extra al cif, nif y nie

--- app/Rules/Nif.php
+++ app/Rules/Nif.php
@@ -27,15 +27,19 @@ class Nif implements Rule
     {
         $original = strtoupper($value);
 
-        if (preg_match('/(\d{8}[TRWAGMYFPDXBNJZSQVHLCKE]{1})/', $original)) {
-            $dni = (int) substr($original, 0, -1);
+        if (ctype_alnum($original)) {
+            if (strlen($original) === 9) {
+                if (preg_match('/(\d{8}[TRWAGMYFPDXBNJZSQVHLCKE]{1})/', $original)) {
+                    $dni = (int) substr($original, 0, -1);
 
-            $portion = $dni % 23;
+                    $portion = $dni % 23;
 
-            $letter = substr("TRWAGMYFPDXBNJZSQVHLCKE", $portion, 1);
+                    $letter = substr("TRWAGMYFPDXBNJZSQVHLCKE", $portion, 1);
 
-            return $letter == substr($original, -1);
-        } 
+                    return $letter == substr($original, -1);
+                }
+            }
+        }
 
         return false;
     }
